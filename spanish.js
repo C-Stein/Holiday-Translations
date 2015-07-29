24e192ed8a810ea5e765d0b4c9bf106736d2f99c
Peter Connolly <pmconnolly80@gmail.com>
2015-07-29 14:39:35 -0500

    spanish bare bones

---
+++ spanish.js
@@ -0,0 +1,24 @@
+var Translate = (function() {
+  var spanishMatch = {
+    "merry":"alegre", 
+    "christmas":"navidad", 
+    "and":"y", 
+    "happy":"feliz", 
+    "new":"nuevo", 
+    "year":"año"}
+};
+
+  return {
+    getSpanishMatch: function() {
+  
+    return spanishMatch;
+    };
+
+    translateToSpanish: function(espanol) {
+      
+    }
+
+
+},
+})();
+
