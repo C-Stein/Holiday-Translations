62329a5700589a0426a26fec889a525a292c2d5b
Caitlin Stein <caitlin.teter@gmail.com>
2015-07-30 09:21:23 -0500

    script file calls correct language and displays translation

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 var $textarea;
-function grabText() {
-	$textarea = $("#textarea").val();
-}
 var $button = $("#button")
+var $translation = $("#translation");
+var $selectedLanguage;
+
+function grabText() {
+  $textarea = $("#textarea").val();
+  $selectedLanguage = $("#language").val();
+
+  if ($selectedLanguage === "French") {
+  	console.log("French slected")
+  	$translation.text(Translate.translateToFrench($textarea));
+  } else if ($selectedLanguage === "German") {
+    $translation.text(Translate.translateToGerman($textarea));
+  } else if ($selectedLanguage === "Spanish") {
+  	$translation.text(Translate.translateToSpanish($textarea));
+  }
+  
+}
+
+
 
 $(document).ready(function() {
 
   $button.click(grabText);
+
 
 });
