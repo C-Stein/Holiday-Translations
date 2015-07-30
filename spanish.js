7cfbed0b5f7aff6c513fe2dc02d381f4ad62a318
Caitlin Stein <caitlin.teter@gmail.com>
2015-07-29 20:51:29 -0500

    got the translate function to work!

--- spanish.js
+++ spanish.js
@@ -14,8 +14,29 @@ var Translate = (function() {
       return spanishMatch;
     },
 
-    translateToSpanish: function(espanol) {
-      var wordArray = espanol.split(" ");
+    translateToSpanish: function(stuff) {
+       	//turn text into array of words
+    
+      var newWordArray = [];
+      var wordArray = stuff.split(" ");
+      console.log(wordArray[0]);
+         console.log(wordArray)
+  	//match text words (from array) with new language words
+  	//ITERATE OVER OBJECT
+      for (var i = 0; i < wordArray.length; i++) {
+        for (var prop in spanishMatch) {
+          if (prop === wordArray[i]) {
+          	newWordArray.push(spanishMatch[prop]);
+          }
+
+        }
+	  }
+  	     console.log("newWordArray " + newWordArray)
+
+  	//turn new language words into readable text
+      var output = newWordArray.join(" ");
+
+      return output;
     }
   }
 })();
