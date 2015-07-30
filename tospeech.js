87d0853145240b64e7ce961c40a7e3691d55a338
Lewis Wade <lewis@mrwade.net>
2015-07-30 10:44:57 -0500

    Added text to speech

---
+++ tospeech.js
@@ -0,0 +1,33 @@
+    $(function() {
+        $('a.say').on('click', function(e){
+            e.preventDefault();
+
+         var getLanguage = $('#language: selected').val();
+         var language;      
+
+         var text = $('input[name="textarea"]').val();
+
+         if(getLanguage === "French") {
+             language = "fr";
+         } 
+         else if (getlanguage === "German") {
+             language = "de"; 
+           }
+         else if (getLanguage === "Spanish") {
+             language = "es";
+         }
+        else
+            return;
+
+
+         text = encodeURICompnent(text);
+         console.log(language);
+        
+          var url = "http://translate.google.com/translate_tts?ie=UTF-8&q=" + text + "&tl=" + language + "";
+        //Sends audio to page 
+          $('audio').attr('src', url).get(0).play();
+    });
+
+   });
+
+
