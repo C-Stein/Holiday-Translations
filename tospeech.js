dcb0a553dcba6f5b53c2139de59d67e2fef75764
Lewis Wade <lewis@mrwade.net>
2015-07-30 11:08:24 -0500

    Removed Accents in ToSpeech due to bugs and time constraints

--- tospeech.js
+++ tospeech.js
@@ -7,8 +7,8 @@
 
          var text = $('input[name="textarea"]').val();
 
-         if($selectedLanguage === "French") {
-             language = "fr";
+       /* removing accents due to bugs   if($selectedLanguage === "French") {
+             language = "de";
          } 
          else if ($selectedLanguage === "German") {
              language = "de"; 
@@ -18,13 +18,16 @@
          }
         else
             return;
-
+*/
 
          text = encodeURIComponent(text);
+         console.log(text);
          console.log(language);
-        
-          var url = "http://translate.google.com/translate_tts?ie=UTF-8&q=" + text + "&tl=" + language + "";
-        //Sends audio to page 
+       
+          var url = "http://translate.google.com/translate_tts?ie=UTF-8&q=" + text + "&tl=en";
+       
+          console.log(url);
+          //Sends audio to page 
           $('audio').attr('src', url).get(0).play();
     });
 
